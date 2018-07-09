334da523388991f786761345af6b59427f0956d3
Wayne0813 <L_Wayne@163.com>
2018-07-09 14:26:31 +0800

    change file

--- weixin/src/main/java/com/lv/test/Test.java
+++ weixin/src/main/java/com/lv/test/Test.java
@@ -8,6 +8,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hah");
         System.out.println("haha");
+        System.out.println("Hello, word!");
 
     }
 }
